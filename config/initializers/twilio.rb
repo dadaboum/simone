14d577e6c7c99ea3f75b0ffe466de34e876c0b6f
jonathan joubert <jjoubert@mandarine-gestion.com>
2018-03-20 12:50:23 +0100

    figaro with environment variable

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,4 +1,4 @@
 Twilio.configure do |config|
-  config.account_sid = Rails.application.secrets.twilio_account_sid
-  config.auth_token = Rails.application.secrets.twilio_auth_token
+  config.account_sid = ENV["TWILIO_ACCOUNT_SID"]
+  config.auth_token = ENV["TWILIO_AUTH_TOKEN"]
 end
