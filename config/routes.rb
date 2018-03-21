8ee547069a0402a6a81249e5b13b0bbe38d4237c
David Benamran <davidbenamran@gmail.com>
2018-03-21 14:20:27 +0100

    side bar done, still need to put the name of the user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :forms, only: [ :index, :new, :create]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :surgeries, only: [ :index, :show ]
 end
