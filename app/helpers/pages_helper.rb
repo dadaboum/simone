dbcbff7b88191e1df1e897c38d505409e86631bd
jonathan joubert <jjoubert@mandarine-gestion.com>
2018-03-22 13:03:40 +0100

    title css ok

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,8 +1,14 @@
 module PagesHelper
+  def nb_pre_op_orange_flag
+    Surgery.where("is_done=false").count
+  end
   def nb_pre_op_yellow_flag
     Surgery.where("is_done=false and pre_form_answered=false").count
   end
   def nb_pre_op_green_flag
-    Surgery.where("is_done=true and pre_form_answered=true").count
+    Surgery.where("is_done=false and pre_form_answered=true").count
+  end
+  def nb_pre_op_red_flag
+    0
   end
 end
