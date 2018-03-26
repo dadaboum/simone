d856895bcd3021a036d3079fd344fb3e799d4c0a
Paul Ehrhardt <paul.ehrhardt@hec.edu>
2018-03-26 17:08:51 +0200

    multiple selection and validate

--- app/javascript/components/card_multiselection.js
+++ app/javascript/components/card_multiselection.js
@@ -1,11 +1,26 @@
-function ready() {
-  $('input[type=checkbox]').click(function () {
-    if ($(this).parent().hasClass('active')) {
-      $(this).parent().removeClass('active');
+const toggleActiveAndInput = (event) => {
+  event.currentTarget.parentNode.classList.toggle("active");
+  const target_surgery_id = event.currentTarget.dataset.surgeryid;
+  const hiddenInput = document.querySelector('#validate_batch_input');
+  if (hiddenInput.value.includes(target_surgery_id)){
+    const array = hiddenInput.value.split(",");
+    const index = array.indexOf(target_surgery_id);
+    array.splice(index, 1);
+    hiddenInput.value = array;
+  } else {
+    if (hiddenInput.value === "") {
+      hiddenInput.value = target_surgery_id;
+    } else {
+      hiddenInput.value += `,${target_surgery_id}`;
     }
-    else
-    { $(this).parent().addClass('active'); }
-  });
-}
+  }
+};
+
+function ready() {
+  const inputs = document.querySelectorAll('input[type=checkbox]');
+  inputs.forEach((input) => {
+    input.addEventListener("click", toggleActiveAndInput)
+  })
+};
 
 export { ready };
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   post '/new-response-webhook', to: 'webhooks#receive', as: :receive_webhooks
+  patch '/validate-batch', to: 'surgeries#validate_batch', as: :validate_batch
 
   resources :forms, only: [ :index, :new, :create, :update]
 
