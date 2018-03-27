d6d2eb680b17061ab8c8224f3ec6c6bace388a97
Paul Ehrhardt <paul.ehrhardt@hec.edu>
2018-03-27 15:41:45 +0200

    design for filters and action buttons + beginning select all

--- app/javascript/components/card_multiselection.js
+++ app/javascript/components/card_multiselection.js
@@ -1,31 +1,49 @@
 const toggleActiveAndInput = (event) => {
-  event.currentTarget.parentNode.classList.toggle("active");
-  const target_surgery_id = event.currentTarget.dataset.surgeryid;
-  const hiddenInput = document.querySelector('#validate_batch_input');
-  if (hiddenInput.value.includes(target_surgery_id)){
-    const array = hiddenInput.value.split(",");
-    const index = array.indexOf(target_surgery_id);
-    array.splice(index, 1);
-    hiddenInput.value = array;
-  } else {
-    if (hiddenInput.value === "") {
-      hiddenInput.value = target_surgery_id;
+  if (event.currentTarget.name !== "select-all") {
+    event.currentTarget.parentNode.classList.toggle("active");
+    const target_surgery_id = event.currentTarget.dataset.surgeryid;
+    const hiddenInput = document.querySelector('#validate_batch_input');
+    if (hiddenInput.value.includes(target_surgery_id)){
+      const array = hiddenInput.value.split(",");
+      const index = array.indexOf(target_surgery_id);
+      array.splice(index, 1);
+      hiddenInput.value = array;
     } else {
-      hiddenInput.value += `,${target_surgery_id}`;
+      if (hiddenInput.value === "") {
+        hiddenInput.value = target_surgery_id;
+      } else {
+        hiddenInput.value += `,${target_surgery_id}`;
+      }
     }
+    const hiddenInputs = document.querySelectorAll('#batch_input');
+    hiddenInputs.forEach(function(e){
+      e.value = hiddenInput.value;
+    });
   }
-  const hiddenInputs = document.querySelectorAll('#batch_input');
-  hiddenInputs.forEach(function(e){
-    e.value = hiddenInput.value;
-  });
-
 };
 
 function ready() {
   const inputs = document.querySelectorAll('input[type=checkbox]');
   inputs.forEach((input) => {
-    input.addEventListener("click", toggleActiveAndInput)
+    input.addEventListener("click", toggleActiveAndInput);
   })
 };
 
 export { ready };
+
+const toggleAll = (event) => {
+  const inputs = document.querySelectorAll('input[type=checkbox]');
+  inputs.forEach((input) => {
+    if (input.name !== "select-all") {
+      input.click();
+    }
+  })
+  console.log("Entrée fonction")
+}
+
+function selectAll() {
+  const check = document.querySelector('input[type=checkbox]');
+  check.addEventListener("click", toggleAll);
+};
+
+export { selectAll };
