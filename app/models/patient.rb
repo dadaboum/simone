9627fbf8d6bbdc708c97d15a607c3f8c1d2b8ff1
Paul Ehrhardt <paul.ehrhardt@hec.edu>
2018-03-28 10:40:41 +0200

    searchbar that multisearch on patient and doctors names

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,7 @@
 class Patient < ApplicationRecord
   has_many :surgeries
   belongs_to :hospital
+
+  include PgSearch
+  multisearchable :against => [:first_name, :last_name]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180327125108) do
+ActiveRecord::Schema.define(version: 20180327155547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,15 @@ ActiveRecord::Schema.define(version: 20180327125108) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["hospital_id"], name: "index_patients_on_hospital_id"
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
   end
 
   create_table "surgeons", force: :cascade do |t|
