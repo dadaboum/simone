71f9839bc053b5bcd65c28fb516708a88fa43f92
jonathan joubert <jjoubert@mandarine-gestion.com>
2018-03-29 14:21:30 +0200

    new name seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,7 +19,7 @@ puts "Created hospital Clinique du sport"
 simone = User.new(email: "simone@gmail.com", password: "motdepasse")
 simone.hospital = clinique_du_sport
 simone.last_name = "Benamran"
-simone.first_name = "Simone"
+simone.first_name = "Simon"
 simone.save
 
 puts "Created Nurse Simone"
