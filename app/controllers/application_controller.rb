e206de52033ebd02d65202b39389e07e9c313b29
jonathan joubert <jjoubert@mandarine-gestion.com>
2018-03-30 16:06:44 +0200

    link when log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
 
   protected
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || surgeries_path
+    request.env['omniauth.origin'] || stored_location_for(resource) || dashboard_path
   end
 end
